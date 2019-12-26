4def63d26c39a8ee8166c91b6b5f2230e5fc1364
avophile <douglas.locklin@gmail.com>
2019-12-26 19:14:34 +0200

    Part1 steps 1 and 2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,30 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
 const App = () => {
-    const course = 'Half Stack application development'
+    const course = 'Half Stack Application Development'
+
     const part1 = 'Fundamentals of React'
+    const part2 = 'Using Props to Pass Data'
+    const part3 = 'State of a Component'
+  
     const exercises1 = 10
-    const part2 = 'Using props to pass data'
     const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
-  
+    const exercises3 = 1
+
+    const Header = (props) => {
+        console.log("Header props: " + props.course)
+        return <h1>{props.course}</h1>
+    }
+    
+    const Part = (props) => {
+        console.log("Part props: ", props.part, props.exercise)
+          return (
+            <p>{props.part} {props.exercise}</p>
+          )
+    }
+    
+    const Content = () => {
+        return (
+          <>
+            <Part part={part1} exercise={exercises1}/>
+            <Part part={part2} exercise={exercises2}/>
+            <Part part={part3} exercise={exercises3}/>
+          </>
+        )
+    }
+    
+    const Total = () => {
+        return (
+          <>The total number of exercises is: {exercises1 + exercises2 + exercises3}</> 
+        )
+    }
+    
+
     return (
-      <div>
-        <h1>{course}</h1>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-      </div>
+      <>
+        <Header course={course} />
+        <Content />
+        <Total />
+      </>
     )
-  }
-  
-  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
